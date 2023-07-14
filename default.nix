097f457d689032b9b5ac83053c08b36f32e8ad44
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-07-14 10:43:22 +1000

    added nix tooling and package

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+}:
+
+pkgs.buildGoApplication {
+  pname = "tfmerge";
+  version = "0.1";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+  nativeBuildInputs = [ pkgs.terraform ];
+}
